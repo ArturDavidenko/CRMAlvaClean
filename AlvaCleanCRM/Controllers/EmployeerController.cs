7d24c2ff96eaa43a691c25fa97b736ef149289ca
ArtursDavidenko <artursdavidenko@gmail.com>
2025-01-25 16:28:27 +0200

    create logic when simple emp can see only his owns orders

---
+++ AlvaCleanCRM/Controllers/EmployeerController.cs
@@ -0,0 +1,24 @@
+﻿using AlvaCleanCRM.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlvaCleanCRM.Controllers
+{
+    public class EmployeerController : Controller
+    {
+        private readonly IEmployeerRepository _employeerRepository;
+
+        public EmployeerController(IEmployeerRepository employeerRepository)
+        {
+            _employeerRepository = employeerRepository;
+        }
+
+
+        public async Task<IActionResult> AllOrdersPageOfEmployeer(string employeerId)
+        {
+            var orders = await _employeerRepository.GetAllOrdersOfEmployeer(employeerId);
+
+            return View("AllOrdersOfEmployeer", orders);
+        }
+
+    }
+}
