3fc3556fb835f7e225417f56110be22b10d29668
ArtursDavidenko <artursdavidenko@gmail.com>
2025-02-02 17:54:04 +0200

    change entity customer

--- AlvaCleanCRM/Models/Customer.cs
+++ AlvaCleanCRM/Models/Customer.cs
@@ -3,6 +3,7 @@
     public class Customer
     {
         public Guid Id { get; set; }
+        public string CustomerType { get; set; }
         public string ClientName { get; set; }
         public string CompanyName { get; set; }
         public string ContactPhone { get; set; }
